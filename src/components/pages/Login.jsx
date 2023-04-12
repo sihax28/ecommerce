5d21eb723381363cd5f7a1d2817969935df3d02f
VICTOR ALCAYAGA M <victor.alcayaga@psinet.net>
2023-04-12 18:50:30 -0400

    Ingresp de plugins forms, y arreglo de url API

--- src/components/pages/Login.jsx
+++ src/components/pages/Login.jsx
@@ -1,30 +1,69 @@
 import axios from "axios"
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 import { API_URL } from "../../constants/env";
 import { setToken } from "../../helpers/auth";
+import { LoginTemplate } from "../templates/LoginTemplate";
+
 const Login = () => {
+
+  const nav = useNavigate();
+
+  const [error, setError] = useState();
 
     const handleSubmit = (e) => {
         e.preventDefault();
-
+        setError();
         const data = {
             email: e.target.email.value,
             password: e.target.password.value
         }
 
         axios.post(`${API_URL}/public/login`, data)
-            .then(resp => setToken(resp.data.data.token))
-            .catch(error => console.log(error));
+            .then(resp => {
+              setToken(resp.data.data.token);
+              nav("/");
+            })
+            .catch(error => {
+              setError(error);
+            });
            
     }
 
   return (
-    <div className="pt-16 max-w-256 m-auto">
+    <LoginTemplate>
       <form onSubmit={handleSubmit}>
-        <input type="email" name="email" placeholder="Correo electrónico" required/>
-        <input type="password" name="password" placeholder="Contraseña" required/>
-        <button type="submit">Ingresar</button>
+        <div className="mb-4">
+          <input className="border-gray-300 w-full rounded-2xl"
+            type="email"
+            placeholder="Correo electrónico"
+            name="email"
+            required
+          />
+        </div>
+        <div className="mb-4">
+          <input className="border-gray-300 w-full rounded-2xl"
+            type="password"
+            placeholder="Contraseña"
+            name="password"
+            required
+          />
+        </div>
+        <div className="text-center pt-1 mb-12 pb-1">
+          <button className="bg-gradient w-full py-2 rounded-full text-gray-100" type="submit">
+            Ingresar
+          </button>
+          <Link className="text-gray-500 " to="/registro">
+            ¿Deseas registrarte?
+          </Link>
+        </div>
+        {error && (
+          <p className="text-center p-2 bg-red-100 text-red-800">
+            {error?.response?.data?.data}
+          </p>
+        )}
       </form>
-    </div>
+    </LoginTemplate>
   )
 }
 
